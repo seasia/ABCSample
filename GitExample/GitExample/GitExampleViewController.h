f13d4e5d112a23e0d3173f32b95efce2f94ccda0
Seasia <tmittal@seasiaconsulting.com>
2012-01-09 10:41:02 +0530

    hi

---
+++ GitExample/GitExample/GitExampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  GitExampleViewController.h
+//  GitExample
+//
+//  Created by Tanvi's mac mini on 09/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GitExampleViewController : UIViewController
+
+@end
---
+++ GitExample/GitExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitExample
+//
+//  Created by Tanvi's mac mini on 09/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GitExampleAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GitExampleAppDelegate class]));
+    }
+}
