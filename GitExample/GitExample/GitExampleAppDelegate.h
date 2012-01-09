5dcc946d46ec6e5a1baa6a0db6597d4fd7ae4b21
Seasia <tmittal@seasiaconsulting.com>
2012-01-09 10:30:41 +0530

    hi

---
+++ GitExample/GitExample/GitExampleAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GitExampleAppDelegate.h
+//  GitExample
+//
+//  Created by Tanvi's mac mini on 09/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class GitExampleViewController;
+
+@interface GitExampleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) GitExampleViewController *viewController;
+
+@end
